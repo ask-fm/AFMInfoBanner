408f5bbed2b07dbe438d4e64c802e9ad3bea9e5c
Romans Karpelcevs <karpelcev@gmail.com>
2014-11-18 19:55:14 +0200

    Supporting multiple screen sizes

--- Example/Tests/CVKInfoBannerSpec.m
+++ Example/Tests/CVKInfoBannerSpec.m
@@ -13,29 +13,42 @@
 static NSString *const kShortBannerText = @"Short error text";
 static NSString *const kLongBannerText = @"Longer text which definitely would be shown on multiple lines and wrap.";
 
+static NSString *sanitizedTestName()
+{
+    SPTSpec *currrentSpec = [[NSThread currentThread] threadDictionary][@"SPTCurrentSpec"];
+    NSString *name = [[[currrentSpec.name componentsSeparatedByString:@" test_"] lastObject] stringByReplacingOccurrencesOfString:@"]" withString:@""];
+    return name;
+}
+
 SpecBegin(CVKInfoBanner)
 
-describe(@"CVKInfoBanner", ^{
+describe(@"CVKInfoBanner view", ^{
     __block CVKInfoBanner *banner;
+
+    NSString *(^deviceSpecificName)() = ^NSString *() {
+        return [NSString stringWithFormat:@"%@-%.0f",
+                sanitizedTestName(),
+                CGRectGetWidth([[UIScreen mainScreen] bounds])];
+    };
 
     it(@"should show compact red one", ^{
         banner = [CVKInfoBanner showWithText:kShortBannerText andStyle:CVKInfoBannerStyleRed];
-        expect(banner).will.haveValidSnapshot();
+        expect(banner).will.haveValidSnapshotNamed(deviceSpecificName());
     });
 
     it(@"should show long red one", ^{
         banner = [CVKInfoBanner showWithText:kLongBannerText andStyle:CVKInfoBannerStyleRed];
-        expect(banner).will.haveValidSnapshot();
+        expect(banner).will.haveValidSnapshotNamed(deviceSpecificName());
     });
 
     it(@"should show compact green one", ^{
         banner = [CVKInfoBanner showWithText:kShortBannerText andStyle:CVKInfoBannerStyleGreen];
-        expect(banner).will.haveValidSnapshot();
+        expect(banner).will.haveValidSnapshotNamed(deviceSpecificName());
     });
 
     it(@"should show long green one", ^{
         banner = [CVKInfoBanner showWithText:kLongBannerText andStyle:CVKInfoBannerStyleGreen];
-        expect(banner).will.haveValidSnapshot();
+        expect(banner).will.haveValidSnapshotNamed(deviceSpecificName());
     });
 
     afterEach(^{
